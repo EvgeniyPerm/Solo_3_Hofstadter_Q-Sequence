d17cadcf35842abfe21fee6b38e28f7002c40ad6
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-20 09:23:25 +0500

    Hofstadter's Q-Sequence is a sequence of numbers where every integer above zero has a corresponding q-sequence value. You can determine the q-sequence value from a formula that tells you how far back in the sequence to go and add two values together. The first two values of the sequence are Q(1) = 1 and Q(2) = 1, and every number above 2 can be expressed according to the following formula (where n is your input value): Q(n) = Q(n - Q(n - 1)) + Q(n - Q(n -2))
    Task:
    Given an integer value input, determine and output the corresponding q-sequence value.
    
    Input Format:
    A positive integer value.
    
    Output Format:
    A positive integer value that represents the value in the q-sequence that the input holds.
    
    Sample Input:
    5
    
    Sample Output:
    3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int funQ(int n){
+        if (n<=0) return -1;
+        if (n==1 || n==2) return 1;
+        int[] seq = new int[n+1];
+        seq[1]=1;
+        seq[2]=1;
+        for (int i = 3; i <= n; i++) {
+            seq[i] = seq[i-seq[i-1]]+seq[i-seq[i-2]];
+
+        }
+        return seq[n];
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(funQ(n));
+
     }
 }
